bef09cc5d68bdcdb8be696bacd360f725d93dd15
shoji-tsubasa-infratop <131357847+shoji-tsubasa-infratop@users.noreply.github.com>
2023-04-25 04:07:44 +0000

    [Add]layout

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,7 @@ class BooksController < ApplicationController
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
-      flash[:notice] = "Error"
+      flash[:danger] = "Error"
       redirect_to books_path
     end
   end
@@ -25,12 +25,14 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
     redirect_to book_path(book.id)
   end
   
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to books_path
   end
   
