74422ff428acb9dc7c9802e35f4cb1ae93c37a6e
shoji-tsubasa-infratop <131357847+shoji-tsubasa-infratop@users.noreply.github.com>
2023-04-25 11:17:57 +0000

    [Fix]model of form-with in NewBook

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,19 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book = Book.new
   end
   
   def create
-    @book = Book.new(book_params)
+    @book = Book.new(book_params)#@はバリデーションのため
     if @book.save
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
+      @books = Book.all
+      render :index
       flash[:danger] = "error: can't be blank"
-      redirect_to books_path
+      #redirect_to books_path#要チェック
     end
   end
   
